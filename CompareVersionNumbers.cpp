c08ff3307897dd7e04d8ddd4ae9d942d479d909c
nine-rec <gongshang1990@163.com>
2015-08-31 23:32:27 +0800

    Compare Version Numbers

--- CompareVersionNumbers.cpp
+++ CompareVersionNumbers.cpp
@@ -2,13 +2,54 @@
 //  versionCompare.cpp
 //  LeetCode
 //
-//  Created by gongshang on 15/8/27.
+//  Created by gongshang on 15/8/31.
 //  Copyright (c) 2015年 ninerec. All rights reserved.
 //
 
 class Solution {
 public:
+    vector<int> &split(const string &s, char delim, vector<int> &elems) {
+        stringstream ss(s);
+        string item;
+        int num;
+        while (getline(ss, item, delim)) {
+            elems.push_back(atoi(item.c_str()));
+        }
+        return elems;
+    }
+
+    vector<int> split(const string &s, char delim) {
+        vector<int> elems;
+        split(s, delim, elems);
+        return elems;
+    }
+
+    void trim_zero(vector<int>& v){
+        while (v.size() > 1 && v.back() == 0){
+            v.pop_back();
+        }
+    }
+
     int compareVersion(string version1, string version2) {
-        char* patch = strtok(version1, '.');
+        int ret = 0;
+        vector<int> nums1 = split(version1, '.');
+        vector<int> nums2 = split(version2, '.');
+
+        trim_zero(nums1);
+        trim_zero(nums2);
+
+        for (int i = 0; i < nums1.size() && i < nums2.size(); ++i)
+        {
+            if (nums1[i] != nums2[i])
+            {
+                return (nums1[i] > nums2[i])? 1 : -1;
+            }
+        }
+
+        if (nums1.size() == nums2.size())
+        {
+            return 0;
+        }
+        return (nums1.size() > nums2.size())? 1 : -1;
     }
 };
