1d26dfcd173377d81de6607d28419bedfa56efc2
nine-rec <gongshang1990@163.com>
2016-01-20 10:37:29 +0800

    Update

--- Sql/TripsAndUsers.sql
+++ Sql/TripsAndUsers.sql
@@ -6,8 +6,10 @@
 #  Copyright (c) 2016年 ninerec. All rights reserved.
 #
 
-Select t.Request_at as `Day`, round(SUM(t.Status!="completed")/COUNT(t.Id), 2) as `Cancellation Rate`
-From Users, Trips as t
-Where (t.Client_Id=Users.Users_Id and (t.Request_at BETWEEN '2013-10-01' AND '2013-10-03')
-        and Users.Banned="No" and Users.Role="Client" )
-Group By Day;
+Select 
+    t.Request_at as `Day`, 
+    round(SUM(t.Status!="completed")/COUNT(t.Id), 2) as `Cancellation Rate`
+From Trips as t Inner Join Users as u 
+    on (t.Client_Id=u.Users_Id and u.Banned="No")
+Where t.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+Group By t.Request_at;
