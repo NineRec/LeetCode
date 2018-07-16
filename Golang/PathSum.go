e0f10a54a07bcfd7443be29b14d2f93e35d08128
gongshang <gongshang1990@163.com>
2018-07-16 21:08:34 +0800

    Path Sum

---
+++ Golang/PathSum.go
@@ -0,0 +1,24 @@
+// 112. Path Sum
+//
+// Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that
+// adding up all the values along the path equals the given sum.
+
+package Golang
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, sum int) bool {
+	return false
+}
