3e69b14ad63b4c3cdb43979f9722bd6a7251325b
gongshang <gongshang1990@163.com>
2018-07-17 10:05:54 +0800

    Path Sum

--- Golang/PathSum.go
+++ Golang/PathSum.go
@@ -20,5 +20,14 @@ type TreeNode struct {
  * }
  */
 func hasPathSum(root *TreeNode, sum int) bool {
-	return false
+	if root == nil {
+		return false
+	}
+
+	// leaf
+	if root.Left == nil && root.Right == nil {
+		return root.Val == sum
+	}
+
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
 }
--- Golang/ThirdMaximumNumber.go
+++ Golang/ThirdMaximumNumber.go
@@ -1,6 +1,8 @@
-// Given a non-empty array of integers, return the third maximum number 
-// in this array. If it does not exist, return the maximum number. 
+// Given a non-empty array of integers, return the third maximum number
+// in this array. If it does not exist, return the maximum number.
 // The time complexity must be in O(n).
+
+// Package Golang is for go code on Leetcode.
 package Golang
 
 func thirdMax(nums []int) int {
